e69e6f771dc21722b418f97df84ccc6237505e38
Devansh Bindal <devanshbindal16@gmail.com>
2025-08-30 20:47:19 +0530

    Day 1 Code – A simple C program that reads two integers from the user and displays their sum, difference, product, and quotient using basic input/output functions.
    This simple C program takes two integers as input from the user and calculates:
    
    Sum – addition of the two numbers
    Difference – subtraction of the second number from the first
    Product – multiplication of the numbers
    Quotient – division result (formatted to two decimal places)
    
    It demonstrates the use of scanf() for user input, basic arithmetic operators (+, -, *, /), and formatted output using printf().

---
+++ Day1_b.c
@@ -0,0 +1,16 @@
+//Q2: Write a program to input two numbers and display their sum, difference, product, and quotient.
+
+#include <stdio.h>
+
+int main(){
+    int num1, num2;
+    printf("Enter first number : ");
+    scanf("%d", &num1);
+    printf("Enter second number : ");
+    scanf("%d", &num2);
+    printf("Sum = %d\n", num1+num2);
+    printf("Difference = %d\n", num1-num2);
+    printf("Product = %d\n", num1*num2);
+    printf("Quotient = %.2f\n", num1/num2);
+    return 0;
+}
