d221b89eeb78b518a36b5ed60de98742cad2c8e0
Devansh Bindal <devanshbindal16@gmail.com>
2025-09-17 19:55:05 +0530

    Day 19 Code – C Program to Find the LCM of Two Numbers

---
+++ Day19_a.c
@@ -0,0 +1,23 @@
+//Q37: Write a program to find the LCM of two numbers.
+
+#include <stdio.h>
+
+int main() {
+    int num1, num2, lcm;
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+    
+    int a = num1, b = num2;
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    int gcd = a;
+    lcm = (num1 * num2) / gcd;
+    
+    printf("LCM of %d and %d is: %d\n", num1, num2, lcm);
+    return 0;
+}
