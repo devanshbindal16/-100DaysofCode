2159a3a4732a5a87fa381bd26d8f37d359aa1c05
Devansh Bindal <devanshbindal16@gmail.com>
2025-09-23 12:49:28 +0530

    Day 25 Code – C Program to Print a Right-Aligned Inverted Triangle Pattern

---
+++ Day25_b.c
@@ -0,0 +1,16 @@
+//Q50: Write a program to print the following pattern:
+
+#include <stdio.h>
+
+int main() {
+    for(int i = 5; i >= 1; i--) {
+        for(int space = 5; space > i; space--) {
+            printf("  ");
+        }
+        for(int j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
