fce5b6cd2a526105a603b838cb4422e560202e8f
Devansh Bindal <devanshbindal16@gmail.com>
2025-09-26 12:34:09 +0530

    Day 28 Code – C Program to Print All Prime Numbers from 1 to n

---
+++ Day28_a.c
@@ -0,0 +1,26 @@
+//Q55: Write a program to print all the prime numbers from 1 to n.
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    printf("Prime numbers from 1 to %d are: ", n);
+    for (int num = 2; num <= n; num++) {
+        int isPrime = 1; 
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                isPrime = 0; 
+                break;
+            }
+        }
+        if (isPrime) {
+            printf("%d ", num);
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
